7c7865467e4fc0cb63196ba01e4197c02991e553
ru0ya <viktaangi02@gmail.com>
2023-03-03 10:52:35 +0300

    fixing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import json
 def number_of_subscribers(subreddit):
     """fetches number of subscribers in a subreddit"""
     headers = {'User-Agent': 'Mozilla/5.0'}
-    url = (f'https://www.reddit.com/r/{subreddit}/about.json')
+    url = 'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
