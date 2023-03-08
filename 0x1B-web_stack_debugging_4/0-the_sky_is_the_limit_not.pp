b325fea621ca5c073e00929a506a27b79590697d
ru0ya <viktaangi02@gmail.com>
2023-03-08 11:31:13 +0300

    test

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+exec{ 'fixing-nginx':
+command  =>  'sed-i "s/15/4096" /etc/default/nginx',
+path    =>  '/usr/local/bin/:/bin/'
+}
+
+-> exec{ 'restart-nginx'
+command  =>  'nginx restart',
+path  =>  '/etc/init.d/'
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Increase hard file limit for Holberton user.
+exec { 'increase-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
